c437204a7f8dfb81718fa168776e15d7e3568951
drexfall <drexfall@protonmail.com>
2023-07-23 22:24:06 +0530

    First

---
+++ main.js
@@ -0,0 +1,5 @@
+function submitVote(movie) {}
+window.addEventListener("DOMContentLoaded", () => {
+	const main = document.querySelector("main");
+	document.body.addEventListener("mousemove", e => {});
+});
